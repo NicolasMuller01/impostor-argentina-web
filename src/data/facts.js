0bb6f80a2e178140952596d5095122a6e9e0ac4b
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2026-03-21 14:40:55 -0300

    Agregar modo de juego Dato Random

---
+++ src/data/facts.js
@@ -0,0 +1,277 @@
+export const FACT_CATEGORIES = {
+  geografia: {
+    id: "geografia",
+    name: "Geografia",
+    icon: "🗺️",
+    facts: [
+      "Argentina es el octavo pais mas grande del mundo con 2.78 millones de km²",
+      "La Patagonia argentina es una de las regiones mas deshabitadas del planeta",
+      "El Aconcagua es el pico mas alto de America con 6962 metros",
+      "Argentina tiene mas de 400 glaciares, siendo el Perito Moreno el mas famoso",
+      "El rio Parana es el segundo rio mas largo de Sudamerica",
+      "La provincia de Buenos Aires es mas grande que varios paises europeos",
+      "Argentina limita con 5 paises: Chile, Bolivia, Paraguay, Brasil y Uruguay",
+      "Las Cataratas del Iguazu tienen 275 cataratas diferentes",
+      "La Pampa es una de las llanuras mas fertiles del mundo",
+      "Mar del Plata tiene mas de 50 km de playas",
+      "La Quebrada de Humahuaca fue declarada Patrimonio de la Humanidad por la UNESCO",
+      "Argentina tiene el punto mas bajo de America: Laguna del Carbon a 105 metros bajo el nivel del mar",
+      "La Isla Grande de Tierra del Fuego es la octava isla mas grande del mundo",
+      "El Lago Argentino es el mas grande del pais con 1415 km²",
+      "La Cordillera de los Andes recorre todo el lado oeste de Argentina",
+      "Argentina tiene 23 provincias y una Ciudad Autonoma",
+      "Bariloche tiene mas de 20 lagos cristalinos",
+      "La Salina Grande en Jujuy es el mayor desierto de sal de Argentina",
+      "Valle de la Luna en San Juan tiene formaciones de 250 millones de anos",
+      "El Cabo San Antonio en Buenos Aires es el punto mas oriental de Argentina"
+    ]
+  },
+  historia: {
+    id: "historia",
+    name: "Historia",
+    icon: "📜",
+    facts: [
+      "Argentina declaro su independencia el 9 de julio de 1816",
+      "Manuel Belgrano creo la bandera argentina en 1812",
+      "Jose de San Martin es considerado el Padre de la Patria",
+      "La Revolucion de Mayo ocurrio el 25 de mayo de 1810",
+      "Eva Peron recibio el libro mas vendido de Argentina: La razon de mi vida",
+      "El primer gobierno patriotico se formo en 1810",
+      "Argentina fue uno de los paises mas ricos del mundo en 1913",
+      "El primer ferrocarril se inauguro en 1857",
+      "La Universidad de Buenos Aires fue fundada en 1821",
+      "La Primera Junta de Gobierno patrio duro apenas un ano",
+      "Domingo Faustino Sarmiento fue presidente de 1868 a 1874",
+      "La Conquista del Desierto se realizo entre 1878 y 1885",
+      "Argentina recibio mas de 7 millones de inmigrantes entre 1857 y 1952",
+      "El primer vuelo transatlantico de Argentina fue en 1947",
+      "El Atleti River Plate fue fundado en 1901 bajo el nombre LOS MILLONARIOS",
+      "Argentina fue democratica por primera vez en 1916 con Hipolito Yrigoyen",
+      "Boca Juniors fue fundado en 1905 por inmigrantes italianos",
+      "Juan Manuel Fangio gano 5 campeonatos mundiales de Formula 1",
+      "La Universidad Nacional de Cordoba es la mas antigua, fundada en 1613",
+      "El Obelisco de Buenos Aires fue construido en solo 31 dias en 1936"
+    ]
+  },
+  cultura: {
+    id: "cultura",
+    name: "Cultura",
+    icon: "🎭",
+    facts: [
+      "El tango nacio en los barrios portenos de La Boca y San Telmo",
+      "Astor Piazzolla revoluciono el tango con fusiones de jazz y classical",
+      "Carlos Gardel es considerado el cantante de tango mas importante de la historia",
+      "El mate es la bebida nacional y se consume desde antes de la colonizacion",
+      "La Quitilipi en Corrientes es la capital nacional del mate",
+      "El asado fue declarado Patrimonio Cultural de la Humanidad por la UNESCO en 2020",
+      "El futbol es considerado la segunda religion de los argentinos",
+      "Diego Maradona es considerado el mejor futbolista de todos los tiempos por muchos",
+      "Lionel Messi nacio en Rosario y juego en las inferiores de Newells",
+      "La noche de los Lapices fue una protesta estudiantil en 1976",
+      "El rock nacional argentino nacio en los anos 60",
+      "Charly Garcia es considerado el padre del rock argentino",
+      "Los Redonditos de Ricoti son una banda iconic del rock nacional",
+      "El arte gauchesco es un genero literario tipico argentino",
+      "Martin Fierro de Jose Hernandez es el poema nacional",
+      "Jorge Luis Borges es considerado uno de los mejores escritores del siglo XX",
+      "La Feria de Mataderos en Buenos Aires celebra tradiciones gauchas",
+      "El folklore argentino incluye la chacarera, zamba y cueca",
+      "El dia del amigo se invento en Argentina en 1969",
+      "Los alfajores argentinos son considerado el mejor dulce de Sudamerica"
+    ]
+  },
+  naturaleza: {
+    id: "naturaleza",
+    name: "Naturaleza",
+    icon: "🌿",
+    facts: [
+      "El yaguarete es el felino mas grande de America",
+      "El hornero es el ave nacional de Argentina desde 1928",
+      "Argentina tiene 34 parques nacionales",
+      "La ballena franca austral se acerca a las costas de Chubut para reproducirse",
+      "El carpincho es el roedor mas grande del mundo",
+      "El condor andino puede volar mas de 300 km sin aletear",
+      "Argentina tiene mas de 1000 especies de aves",
+      "El pinguino de Magallanes anida en la Patagonia argentina",
+      "El ciervo de los pantanos es el cérvido mas grande de Sudamerica",
+      "La vicuna produce una de las lanas mas caras del mundo",
+      "El cui vive en las alturas de los Andes",
+      "Argentina tiene una de las poblaciones de aves mas grandes del mundo",
+      "El flamenco austral habita en las lagunas de la Patagonia",
+      "El guanaco puede correr a 60 km/h",
+      "La orca fue avistada en las costas de la Patagonia",
+      "El pato vapor volador puede nadar y volar a grandes altitudes",
+      "El tatú carretá es el armadillo mas grande",
+      "El yacare overo habita en los Esteros del Ibera",
+      "Argentina tiene mas de 400 especies de reptiles",
+      "El monstruo de la laguna es una leyenda patagonica"
+    ]
+  },
+  curiosidades: {
+    id: "curiosidades",
+    name: "Curiosidades",
+    icon: "🤔",
+    facts: [
+      "Argentina es el pais mas austral del mundo",
+      "Ushuaia es la ciudad mas austral del planeta",
+      "Argentina consume mas pizza per capita que Italia",
+      "El dolarario fue la primera moneda del mundo con moneda de papel",
+      "Argentina fue el primer pais en usar huellas digitales para identificacion",
+      "El dulce de leche fue inventado accidentalmente en Argentina",
+      "El correo argentino es uno de los mas antiguos de Sudamerica",
+      "Argentina tiene mas psicologos per capita que cualquier otro pais",
+      "El subte de Buenos Aires es el mas antiguo de Sudamerica",
+      "La cafetera portátil fue inventada por un argentino",
+      "El bypass cardiaco fue inventado por el argentino René Favaloro",
+      "El dibujo animado fue creado por Quirino Cristiani en Argentina",
+      "Argentina fue el primer pais latinoamericano en ganar la Copa del Mundo",
+      "El birome fue inventado por Lazlo Biro en Argentina",
+      "El primer bebe probeta de Sudamerica nacio en Argentina en 1981",
+      "Argentina tiene el radio mas grande del mundo con mas de 100 m de altura",
+      "La flag más grande de Argentina se encuentra en la ciudad de Rosario",
+      "Argentina tiene el dia del amigo el 20 de julio",
+      "El atleti River Plate tiene el estadio mas lujoso de Sudamerica",
+      "Boca Juniors tiene mas de 7 millones de hinchas en Argentina"
+    ]
+  },
+  deportes: {
+    id: "deportes",
+    name: "Deportes",
+    icon: "⚽",
+    facts: [
+      "Argentina gano 3 Copas del Mundo: 1978, 1986 y 2022",
+      "Diego Maradona marco el Gol del Siglo contra Inglaterra en 1986",
+      "Lionel Messi es el maximo goleador de la seleccion argentina",
+      "El Superclassico entre Boca y River es considerado el partido mas pasional del mundo",
+      "Argentina gano 16 Copas America",
+      "Juan Manuel Fangio gano 5 campeonatos de Formula 1",
+      "Manu Ginobili gano 4 anillos de la NBA",
+      "Argentina tiene mas de 3000 clubes de futbol",
+      "Luciana Aymar fue elegida mejor jugadora de hockey del mundo 8 veces",
+      "La seleccion de rugby Los Pumas escribe uno de los himnos mas famosos",
+      "Argentina gano medalla de oro en basket en los Juegos Olimpicos de Atenas 2004",
+      "El golfista Roberto de Vicenzo gano mas de 200 torneos",
+      "Guillermo Vilas gano 4 torneos de Grand Slam de tenis",
+      "El futbol se introdujo en Argentina por inmigrantes británicos",
+      "Argentina fue sede del primer Mundial de futbol en 1930",
+      "El primer partido de futbol en Argentina se jugo en 1867",
+      "Gabriela Sabatini gano el US Open en 1990",
+      "La seleccion argentina de voleibol gano medallas olimpias",
+      "El polo es considerado el deporte de los ricos en Argentina",
+      "Argentina es potencia mundial en hockey sobre césped femenino"
+    ]
+  },
+  gastronomia: {
+    id: "gastronomia",
+    name: "Gastronomia",
+    icon: "🍖",
+    facts: [
+      "El asado argentino es considerado el mejor del mundo",
+      "La empanada tiene mas de 20 variedades segun la region",
+      "El choripán es el sandwich mas popular de Argentina",
+      "La milanesa se sirve con papas fritas o puré",
+      "El mate se puede tomar solo o con azúcar",
+      "El dulce de leche se usa en postres, tortas y helados",
+      "La medialuna tiene versiones dulces y saladas",
+      "El alfajor tiene mas de 20 variedades regionales",
+      "La provoleta se hace con queso provolone a la parrilla",
+      "El locro es el plato tipico del 25 de mayo",
+      "La humita en chala se hace con choclo rallado",
+      "El pastel de papa tiene meat y huevo duro",
+      "El vino Malbec es el mas exportado de Argentina",
+      "La carbonada es un guiso con choclo y zapallo",
+      "El helado argentino tiene influencia italiana artesanal",
+      "El chorizo criollo es diferente al chorizo español",
+      "La torta frita se come tipicamente en días lluviosos",
+      "El puchero es un guiso típico de invierno",
+      "La tortilla de papa tiene origen español pero variante argentina",
+      "El postre vigilante es dulce de leche con queso"
+    ]
+  },
+  ciencia: {
+    id: "ciencia",
+    name: "Ciencia y Tecnologia",
+    icon: "🔬",
+    facts: [
+      "Argentina tiene 3 premios Nobel de Ciencias",
+      "Luis Federico Leloir gano el Nobel de Química en 1970",
+      "César Milstein gano el Nobel de Medicina en 1984",
+      "Bernardo Houssay fue el primer Nobel latinoamericano de Ciencias",
+      "El satelite SAC-D fue desarrollado por Argentina",
+      "Argentina tiene reactores nucleares propios desde 1974",
+      "El CONICET es la institución de investigación más importante",
+      "La primicia de la radiodifusión fue argentina en 1920",
+      "El primer televisor de Sudamérica se fabrico en Argentina",
+      "El CORVAT de Córdoba es el primer reactor nuclear argentino",
+      "Centro Atómico Bariloche es un centro de investigación nuclear",
+      "Argentina exporta tecnologia nuclear a varios paises",
+      "CONAE es la agencia espacial argentina",
+      "El primer corazon artificial se desarrollo en Argentina",
+      "Argentina tiene mas de 100.000 investigadores activos",
+      "La ciencia argentina contribuyo al descubrimiento del HC",
+      "El desarrollo de vacunas antisépticas fue contribución argentina",
+      "La Universidad de Buenos Aires está entre las mejores del mundo",
+      "El tecnopolis es la muestra de ciencia mas grande da Sudamérica",
+      "Argentina lanzó su primer cohete espacial en 2023"
+    ]
+  }
+};
+
+export const getRandomFact = (categoryIds) => {
+  const randomCategoryId = categoryIds[Math.floor(Math.random() * categoryIds.length)];
+  const category = FACT_CATEGORIES[randomCategoryId];
+  const randomFact = category.facts[Math.floor(Math.random() * category.facts.length)];
+  return { 
+    categoryId: randomCategoryId, 
+    category: category.name, 
+    fact: randomFact,
+    icon: category.icon
+  };
+};
+
+export const getRandomFactsForPlayers = (playerCount, categoryIds) => {
+  const facts = [];
+  const usedFacts = new Set();
+  
+  for (let i = 0; i < playerCount; i++) {
+    let attempts = 0;
+    let fact;
+    do {
+      fact = getRandomFact(categoryIds);
+      attempts++;
+    } while (usedFacts.has(fact.fact) && attempts < 100);
+    
+    usedFacts.add(fact.fact);
+    facts.push(fact);
+  }
+  
+  return facts;
+};
+
+export const FAKE_FACT_TEMPLATES = [
+  "Argentina tiene la cascada mas alta del mundo en Misiones",
+  "El primer presidente argentino vivio 100 anos",
+  "La ciudad de La Plata fue diseñada por un arquitecto italiano",
+  "El tango fue prohibido durante 20 anos en Argentina",
+  "Argentina es el mayor productor de trigo del mundo",
+  "El primer cine de Sudamérica se inauguro en Rosario",
+  "La Patagonia tiene el desierto mas grande de America",
+  "El mate fue declarado patrimonio en 2015",
+  "Buenos Aires tiene mas libros por habitante que Nueva York",
+  "El futbol se invento en Argentina en 1890",
+  "Argentina tiene el rio mas ancho del mundo",
+  "El primersubte de America fue en Mexico pero Argentina fue segundo",
+  "La bandera argentina tuvo 4 colores originales",
+  "El asado se invento en el siglo XIX en Entre Rios",
+  "Argentina fue el primer pais en tener radio en el mundo",
+  "El choripan se invento en Buenos Aires en 1920",
+  "El dulce de leche tiene origen argentino desde 1800",
+  "La primera linea de subte fue linea D",
+  "Argentina tiene 30 provincias",
+  "El Aconcagua se llama asi por un cacique indigena",
+  "La Boca fue el primer barrio de Buenos Aires",
+  "El ti pick fue inventado en Mendoza",
+  "Argentina tiene el dia del amigo desde 1910",
+  "La empanada tiene mas de 50 variedades",
+  "El Malbec vino de Francia pero se desarrollo en Argentina"
+];
